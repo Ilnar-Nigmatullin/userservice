9725d9998b338dbb5c9c92e6c0be77e546cf6d54
Ilnar-Nigmatullin <ilnar.nigmat@gmail.com>
2021-12-23 00:51:32 +0300

    first commit

---
+++ src/main/java/io/getarrays/userservice/api/UserResource.java
@@ -0,0 +1,23 @@
+package io.getarrays.userservice.api;
+
+import io.getarrays.userservice.domain.User;
+import io.getarrays.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserResource {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+}
